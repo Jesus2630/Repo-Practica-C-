64a4da00c89a7110f185d1df92cdabb5f4e9432a
Jesus Farias <jesus.fg2630@gmail.com>
2022-09-27 13:43:48 -0300

    operadores básicos

--- Program.cs
+++ Program.cs
@@ -10,7 +10,37 @@ namespace Seccion2
     {
         static void Main(string[] args)
         {
-           
+            int numero1 =  8;
+            double numero2 = 4.2;
+            double resultado;
+
+            //Sumamos
+            resultado = numero1 + numero2;
+
+            //Operadores Aritmeticos
+
+            //Concatenación
+            string saludo = "Hola";
+            string nombre = "Jesús";
+
+            //Operadores Básicos
+            /* Console.WriteLine(20 - 2);
+            Console.WriteLine(20 + 2);
+            Console.WriteLine(20 * 2);
+            Console.WriteLine(20 / 2); */
+
+            //Operador de División
+
+            //Float
+            Console.WriteLine(16.8f / 4.1f);
+            //Double
+            Console.WriteLine(16.8d / 4.1d);
+            //Decimal
+            Console.WriteLine(16.8m / 4.1m);
+
+            //Resto
+            Console.WriteLine(5 % 2);
+            Console.WriteLine(5.9 % 3.1);
         }
     }
 }
