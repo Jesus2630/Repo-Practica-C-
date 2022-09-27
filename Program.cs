1b7baa1e199ce7d23daf8c975d5fd32861a6eb59
Jesus Farias <jesus.fg2630@gmail.com>
2022-09-27 20:55:40 -0300

    Ejercicio de práctica, area y perimetro de un rectangulo

--- Program.cs
+++ Program.cs
@@ -10,7 +10,26 @@ namespace Seccion2
     {
         static void Main(string[] args)
         {
-           
+            //Realizar un programa que haga el calculo del área y perímetro de un rectangulo
+            double altura, ancho, area, perimetro;
+
+
+            //Pedimos la altura y leemos la altura
+            Console.Write("Ingrese la altura: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            //Pedimos la base y leemos la base
+            Console.Write("Ingrese el ancho: ");
+            ancho = Convert.ToDouble(Console.ReadLine());
+
+            //Calculamos el Área
+            area = altura * ancho;
+
+            //Calculamos el Perímetro
+            perimetro = 2 * (altura + ancho);
+
+            //Mostramos el resultado
+            Console.WriteLine("El aréa de su rectangulo es {0} y el perímetro {1}", area, perimetro);
         }
     }
 }
