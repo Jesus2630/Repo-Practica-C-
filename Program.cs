72726f83e2f1bf65b7daf801de39cf7ba118c4fb
Jesus Farias <jesus.fg2630@gmail.com>
2022-09-28 18:47:26 -0300

    Conversión centigrados

--- Program.cs
+++ Program.cs
@@ -10,7 +10,19 @@ namespace Seccion2
     {
         static void Main(string[] args)
         {
-           
+            //Declaramos las variables a utilizar
+            double centigrados;
+            double fahrenheit;
+
+            //Pedimos la temperatura a convertir
+            Console.Write("Ingrese la temperatura a convertir: ");
+            centigrados = Convert.ToDouble(Console.ReadLine());
+
+            //Hacemos el calculo
+            fahrenheit = (1.8 * centigrados) + 32;
+
+            //Mostramos el resultado
+            Console.WriteLine("La temperatura ingresada son {0} en Fahrenheit", fahrenheit);
         }
     }
 }
