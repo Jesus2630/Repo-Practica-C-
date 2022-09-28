718d29f7152a92c47b6a96e5418f62273b986fd6
Jesus Farias <jesus.fg2630@gmail.com>
2022-09-28 18:12:43 -0300

    Conversión de poligono regular

--- Program.cs
+++ Program.cs
@@ -10,7 +10,26 @@ namespace Seccion2
     {
         static void Main(string[] args)
         {
-           
+            
+            //Programa para calcular el perimetro de cualquier poligono regular. 
+
+            int lados;
+            double tamaño, perimetro;
+
+            //Pido cantidad de lados
+            Console.Write("Cuantos lados tiene tu polígono: ");
+            lados = Convert.ToInt32(Console.ReadLine());
+
+            //Pido el tamaño de los lados
+            Console.Write("Ingrese el tamaño de los lados: ");
+            tamaño = Convert.ToDouble(Console.ReadLine());
+
+            //Calculo el perimetro
+            perimetro = tamaño * lados;
+
+            //Muestro el resultado
+            Console.WriteLine("El perimetro de su poligono es: {0}", perimetro);
+
         }
     }
 }
